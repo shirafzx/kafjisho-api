93f51fc0a4461b9d6dcc9949c6260a9e7f46eaa5
shirafzx <shirafzx@shirafzx.local>
2025-01-24 00:08:01 +0700

    :bento: add example query params

--- src/api/controllers/japanese_word_controller.rs
+++ src/api/controllers/japanese_word_controller.rs
@@ -1,11 +1,12 @@
 use std::sync::Arc;
 
 use axum::{
-    extract::{Path, State},
+    extract::{Path, Query, State},
     http::StatusCode,
     response::IntoResponse,
     Json,
 };
+use serde::Deserialize;
 
 use crate::{
     application::use_cases::japanese_word::{
@@ -15,13 +16,21 @@ use crate::{
     domain::repositories::japanese_word::JapaneseWordRepository,
 };
 
+#[derive(Deserialize)]
+pub struct QueryParams {
+    test: Option<String>,
+}
+
 pub async fn get_japanese_word<T>(
     State(get_japanese_word_use_case): State<Arc<GetJapaneseWordUseCase<T>>>,
     Path(kanji): Path<String>,
+    Query(params): Query<QueryParams>,
 ) -> impl IntoResponse
 where
     T: JapaneseWordRepository + Send + Sync,
 {
+    let test_query_param = params.test.unwrap_or_else(|| "default".to_string());
+    println!("{}", test_query_param);
     match get_japanese_word_use_case.get_japanese_word(kanji).await {
         Ok(japanese_word) => (StatusCode::OK, Json(japanese_word)).into_response(),
         Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, Json(err.to_string())).into_response(),
